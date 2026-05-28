4ec18a687af4f41f72155e2fae676bd322d6791d
Cagyo <cagyodev@gmail.com>
2026-05-28 16:12:09 +0300

    refactor(sensors): remove flat structure and wire hexagonal module
    - Delete sensor.interface.ts, sensor.registry.ts, drivers/ and their tests
      (superseded by domain/, application/, and infrastructure/ layers)
    - Update SensorModule to provide SensorRegistryService, ReloadSensorsUseCase,
      SensorDriverFactory, DrizzleSensorRepository, DrizzleSensorLogRepository via
      DI tokens (SENSOR_DRIVER_FACTORY, SENSOR_REPOSITORY, SENSOR_LOG_REPOSITORY)

--- src/sensors/sensor.module.ts
+++ src/sensors/sensor.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
-import { SensorRegistry } from './sensor.registry';
-import { PigpioGateway } from './drivers/pigpio.gateway';
+import { ReloadSensorsUseCase } from './application/reload-sensors.use-case';
+import { SensorRegistryService } from './application/sensor-registry.service';
+import {
+  SENSOR_DRIVER_FACTORY,
+  SensorDriverFactory,
+} from './domain/ports/sensor-driver.port';
+import {
+  SENSOR_LOG_REPOSITORY,
+  SensorLogRepositoryPort,
+} from './domain/ports/sensor-log-repository.port';
+import { SENSOR_REPOSITORY } from './domain/ports/sensor-repository.port';
+import { DrizzleSensorLogRepository } from './infrastructure/drizzle-sensor-log.repository';
+import { DrizzleSensorRepository } from './infrastructure/drizzle-sensor.repository';
+import { PigpioGateway } from './infrastructure/pigpio.gateway';
+import { SensorDriverFactoryProvider } from './infrastructure/sensor-driver.factory';
 
 @Module({
-  providers: [SensorRegistry, PigpioGateway],
-  exports: [SensorRegistry, PigpioGateway],
+  providers: [
+    SensorRegistryService,
+    ReloadSensorsUseCase,
+    PigpioGateway,
+    { provide: SENSOR_REPOSITORY, useClass: DrizzleSensorRepository },
+    { provide: SENSOR_LOG_REPOSITORY, useClass: DrizzleSensorLogRepository },
+    {
+      provide: SENSOR_DRIVER_FACTORY,
+      useFactory: (
+        pigpio: PigpioGateway,
+        sensorLogs: SensorLogRepositoryPort,
+      ): SensorDriverFactory =>
+        SensorDriverFactoryProvider.build({ pigpio, sensorLogs }),
+      inject: [PigpioGateway, SENSOR_LOG_REPOSITORY],
+    },
+  ],
+  exports: [
+    SensorRegistryService,
+    ReloadSensorsUseCase,
+    PigpioGateway,
+    SENSOR_REPOSITORY,
+    SENSOR_LOG_REPOSITORY,
+    SENSOR_DRIVER_FACTORY,
+  ],
 })
 export class SensorModule {}
